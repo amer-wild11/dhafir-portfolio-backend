24fe92bdbc56efe1280304f37c8f97fb002df0a8
amer-wild11 <amer33wild@gmail.com>
2025-01-05 15:36:35 +0300

    v-1

--- server/middleware/cors.global.ts
+++ server/middleware/cors.global.ts
@@ -1,5 +1,5 @@
 export default defineEventHandler((event) => {
-  setHeader(event, "Access-Control-Allow-Origin", "*");
+  setHeader(event, "Access-Control-Allow-Origin", "https://www.dhafir-ayad.com/");
   setHeader(
     event,
     "Access-Control-Allow-Methods",
